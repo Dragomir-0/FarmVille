023d8ba218cbca8933a9a1fe6e037c82851ff54e
Dragomir-0 <39728866+Dragomir-0@users.noreply.github.com>
2018-05-29 15:00:32 +0200

    Add files via upload
    Changeables class

---
+++ BLL/ChangeAbles.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.BLL
+{
+    public static class ChangeAbles
+    {
+        public const int splitOn = ',';
+    }
+}
