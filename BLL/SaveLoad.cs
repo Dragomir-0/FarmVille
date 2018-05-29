bfaa6542c4a9861dd06598ae28fca7a4d1951e6b
Dragomir-0 <39728866+Dragomir-0@users.noreply.github.com>
2018-05-29 14:50:15 +0200

    Add files via upload
    Added SaveLoad

---
+++ BLL/SaveLoad.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FarmVille
+{
+	public class SaveLoad
+	{
+
+		//FarmName
+		//FarmMetaData
+
+		//UserCloths
+		//Farms size
+		
+
+		//Animal+Amount
+
+		public void SaveThis(List<Animal> animals, Farm farm)
+		{
+			List<string> writeMetaData = new List<string>();
+			foreach(Animal a in animals)
+			{
+				writeMetaData.Add();
+			}	
+		}
+
+	}
+}
